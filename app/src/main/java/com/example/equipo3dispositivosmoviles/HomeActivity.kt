d4ac5f6637c4e6a28fa2d6ef132a33f07c4d3494
Brian <briandorantes@hotmail.com>
2022-09-01 02:07:56 -0500

    primer intento de cambio de idioma

--- app/src/main/java/com/example/equipo3dispositivosmoviles/HomeActivity.kt
+++ app/src/main/java/com/example/equipo3dispositivosmoviles/HomeActivity.kt
@@ -1,16 +1,18 @@
 package com.example.equipo3dispositivosmoviles
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import java.util.*
+
 
 enum class ProviderType{
     BASIC
 }
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -25,6 +27,17 @@ class HomeActivity : AppCompatActivity() {
         title = "Inicio"
         val admins =arrayOf("diego-fraga2011@hotmail.com", "dany2001deleon@gmail.com", "briandorantes@hotmail.com")
         val correo = findViewById<TextView>(R.id.EmailtextView)
+
+        //Selector de Idioma
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(this,
+            R.array.language, android.R.layout.simple_spinner_item).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener =this
+
+
         correo.text = email
         val admin = findViewById<TextView>(R.id.adminview)
         if(admins.contains(email)){
@@ -39,5 +52,39 @@ class HomeActivity : AppCompatActivity() {
         }
 
     }
+
+
+
+    //Funcion Cambiar de Idioma
+    private fun setLanguageForApp(language: String) {
+        val locale: Locale
+        if (language == "not-set") {
+            locale = Locale.getDefault()
+        } else {
+            locale = Locale(language)
+        }
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+        if (position == 1){
+            val languague = "en-rUS"
+            setLanguageForApp(languague)
+            startActivity(intent)
+            finish()
+        }else if(position == 0){
+            val languague = "es-rMX"
+            setLanguageForApp(languague)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+    }
 }
+
 
